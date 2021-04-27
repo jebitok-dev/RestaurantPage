8ece1ad7655ba56f6bbed5ffc6a18c54b95b8414
Jebitok <sharonkosgei4@gmail.com>
2021-04-27 13:01:49 +0300

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+/*eslint-disable */
 import { loadPage } from './onload';
 import { renderHomePage } from './homepage';
 import { renderContactUs } from './contact-us';
 import { renderMenuPage } from './menu';
+/*eslint-enable */
 
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
@@ -26,10 +28,11 @@ tabs.forEach((tab) => tab.addEventListener(
     target.classList.add('active');
   }
 ));
+/*eslint quotes: [2, "backtick"]*/
 document.querySelector('.order-now').addEventListener('click', () => {
   document.querySelector(`[data-tab-target='#menu']`).classList.add('red');
 });
-
+/*eslint quotes: [2, "backtick"]*/
 document.querySelector(`[type='submit']`).addEventListener('click', () => {
   document.querySelector('form').reset();
 });
